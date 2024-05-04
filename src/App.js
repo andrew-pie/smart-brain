405887a4e845a9d87470ccc8e4231f607dff45e1
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2024-05-04 15:15:00 -0600

    added FaceRecognition

--- src/App.js
+++ src/App.js
@@ -12,17 +12,38 @@ class App extends Component {
 		super();
 		this.state = {
 			input: '',
-			imageUrl: ''
+			imageUrl: '',
+			box: {},
 		}
 	}
 
 
+	calculateFaceLocation = (data) => {
+		const clarifaiFace = data.result.outputs[0].data.regions[0].region_info.bounding_box;
+		console.log('test2:', clarifaiFace)
+		const image = document.getElementById('inputImage');
+		const width = Number(image.width);
+		const height = Number(image.height);
+		console.log(width, height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - (clarifaiFace.right_col * width),
+			bottomRow: height - (clarifaiFace.bottom_row * height)
+		}
+	}
+
+	displayFaceBox = (box) => {
+		console.log(box);
+		this.setState({ box: box });
+	}
+
 	onInputChange = (event) => {
-		this.setState({input: event.target.value});
+		this.setState({ input: event.target.value });
 	}
 
 	onButtonSubmit = () => {
-		this.setState({imageUrl: this.state.input})
+		this.setState({ imageUrl: this.state.input })
 
 		///////////////////////////////////////////////////////////////////////////////////////////////////
 		// In this section, we set the user authentication, user and app ID, model details, and the URL
@@ -100,13 +121,15 @@ class App extends Component {
 						const value = concept.value.toFixed(4);
 
 						console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+						console.log('boundingBox:', boundingBox);
+						console.log('test:', result.outputs[0].data.regions[0].region_info.bounding_box);
 
 					});
 				});
 
 			})
+			.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
 			.catch(error => console.log('error', error));
-
 	}
 
 	render() {
@@ -121,7 +144,7 @@ class App extends Component {
 						onInputChange={this.onInputChange}
 						onButtonSubmit={this.onButtonSubmit}
 					/>
-					<FaceRecognition imageUrl={this.state.imageUrl}/>
+					<FaceRecognition imageUrl={this.state.imageUrl} />
 				</header>
 			</div>
 		);
