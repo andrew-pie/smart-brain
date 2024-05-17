28bafa615da458ed870f9b93e87ea86330df26a0
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2024-05-17 15:32:46 -0600

    SignIn

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
 			input: '',
 			imageUrl: '',
 			box: {},
+			route: 'signin'
 		}
 	}
 
@@ -144,14 +145,18 @@ class App extends Component {
 				<ParticlesBg type="cobweb" bg={true} />
 				<header className="App-header">
 					<Navigation />
-					<Signin />
-					<Logo />
-					<Rank />
-					<ImageLinkForm
-						onInputChange={this.onInputChange}
-						onButtonSubmit={this.onButtonSubmit}
-					/>
-					<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+					{this.state.route === 'signin'
+						? <Signin />
+						: <div>
+							<Logo />
+							<Rank />
+							<ImageLinkForm
+								onInputChange={this.onInputChange}
+								onButtonSubmit={this.onButtonSubmit}
+							/>
+							<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+						</div>
+					}
 				</header>
 			</div>
 		);
